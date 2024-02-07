ad8931c4785fb6cb2c17a2f4c35da4b2eec46a1f
MoonJaeGyeong <bblbblan69@gmail.com>
2024-02-07 21:15:42 +0900

    :sparkles: 기능 추가 : authController 에 /emailCheck 추가

--- src/main/java/com/c_comachi/inused/domain/users/controller/AuthController.java
+++ src/main/java/com/c_comachi/inused/domain/users/controller/AuthController.java
@@ -1,19 +1,22 @@
 package com.c_comachi.inused.domain.users.controller;
 
 import com.c_comachi.inused.domain.users.dto.request.LoginRequestDto;
+import com.c_comachi.inused.domain.users.dto.request.MailRequestDto;
 import com.c_comachi.inused.domain.users.dto.request.RegisterRequestDto;
 import com.c_comachi.inused.domain.users.dto.request.TokenRequestDto;
+import com.c_comachi.inused.domain.users.dto.response.EmailCheckResponseDto;
 import com.c_comachi.inused.domain.users.dto.response.LoginResponseDto;
 import com.c_comachi.inused.domain.users.dto.response.RegisterResponseDto;
 import com.c_comachi.inused.domain.users.dto.response.ReissueResponseDto;
 import com.c_comachi.inused.domain.users.service.AuthService;
+import com.c_comachi.inused.domain.users.service.MailService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final MailService mailService;
 
     @PostMapping("/register")
     public ResponseEntity<? super RegisterResponseDto> register(@RequestBody @Valid RegisterRequestDto requestBody) {
@@ -36,6 +40,12 @@ public class AuthController {
     @PostMapping("/reissue")
     public ResponseEntity<? super ReissueResponseDto> reissue(@RequestBody @Valid TokenRequestDto requestBody) {
         ResponseEntity<? super ReissueResponseDto> response = authService.reissue(requestBody);
+        return response;
+    }
+
+    @PostMapping("/emailCheck")
+    public ResponseEntity<? super EmailCheckResponseDto> EmailCheck(@RequestBody @Valid MailRequestDto requestBody) throws MessagingException, UnsupportedEncodingException {
+        ResponseEntity<? super EmailCheckResponseDto> response = mailService.sendEmail(requestBody.getEmail());
         return response;
     }
 }
