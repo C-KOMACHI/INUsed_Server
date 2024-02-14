7c286c5ea7d4431630542fafab91c427eb22f89e
MoonJaeGyeong <bblbblan69@gmail.com>
2024-02-15 00:49:59 +0900

    :sparkles: feat  : 닉네임 중복 검사 API 추가

--- src/main/java/com/c_comachi/inused/domain/users/service/AuthService.java
+++ src/main/java/com/c_comachi/inused/domain/users/service/AuthService.java
@@ -7,6 +7,7 @@ import com.c_comachi.inused.domain.users.dto.request.RegisterRequestDto;
 import com.c_comachi.inused.domain.users.dto.request.LoginRequestDto;
 import com.c_comachi.inused.domain.users.dto.response.LoginResponseDto;
 import com.c_comachi.inused.domain.users.dto.response.RegisterResponseDto;
+import com.c_comachi.inused.global.dto.ResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
@@ -15,4 +16,5 @@ public interface AuthService {
     ResponseEntity<? super LoginResponseDto> login(LoginRequestDto loginRequestDto);
     ResponseEntity<? super ReissueResponseDto> reissue(TokenRequestDto tokenRequestDto);
     ResponseEntity<? super LogoutResponseDto> logout(TokenRequestDto tokenRequestDto);
+    ResponseEntity<? super RegisterResponseDto> nicknameCheck(String nickname);
 }
