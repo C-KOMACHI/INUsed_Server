013ce3e1086316e83e3ffe49507d43a16a9e284f
MoonJaeGyeong <bblbblan69@gmail.com>
2024-03-03 23:25:44 +0900

    :sparkles: feat : 양방향 관계 설정

--- src/main/java/com/c_comachi/inused/domain/post/entity/CategoryEntity.java
+++ src/main/java/com/c_comachi/inused/domain/post/entity/CategoryEntity.java
@@ -1,6 +1,8 @@
 package com.c_comachi.inused.domain.post.entity;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +23,9 @@ public class CategoryEntity {
 
     @Column(nullable = false)
     private String image;
+
+    @OneToMany(mappedBy = "category")
+    private List<PostEntity> Posts = new ArrayList<>();
 
     @Builder
     public CategoryEntity(Long id, String name, String image) {
