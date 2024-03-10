b282103f31c4b0df576d19fb0645b44425664a9a
MoonJaeGyeong <bblbblan69@gmail.com>
2024-03-10 15:35:38 +0900

    :sparkles: feat : 로그인 시 id 검증 추가

--- src/main/java/com/c_comachi/inused/domain/users/repository/UserRepository.java
+++ src/main/java/com/c_comachi/inused/domain/users/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.c_comachi.inused.domain.users.repository;
 
 import com.c_comachi.inused.domain.users.entity.UserEntity;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,10 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    @EntityGraph(attributePaths = "authority")
     Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findById(Long id);
+
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 
