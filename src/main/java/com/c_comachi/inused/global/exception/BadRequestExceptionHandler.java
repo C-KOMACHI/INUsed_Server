eb4c7235d1f3a699f5d7d249245aea622944d0e7
MoonJaeGyeong <bblbblan69@gmail.com>
2024-03-10 15:32:19 +0900

    :sparkles: feat : BadRequest 예외 처리

---
+++ src/main/java/com/c_comachi/inused/global/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.c_comachi.inused.global.exception;
+
+import com.c_comachi.inused.global.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BadRequestExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception) {
+        return ResponseDto.validationFailed();
+    }
+}
