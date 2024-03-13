2db8de32b7f6d0d0c72eb52c169afd1b7939fedb
yusica09 <choijiwoo0922@gmail.com>
2024-03-13 20:47:50 +0900

    :sparkles: feat : 문의처리 엔티티 작성

---
+++ src/main/java/com/c_comachi/inused/domain/inquiry/entity/ManagerInquiryEntity.java
@@ -0,0 +1,46 @@
+package com.c_comachi.inused.domain.inquiry.entity;
+
+import com.c_comachi.inused.domain.users.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="manager_inquiry")
+public class ManagerInquiryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "manager_inquiry_id", nullable = false)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "inquiry_id")
+    private UserInquiryEntity inquiry;
+
+    @Builder
+    public ManagerInquiryEntity(Long id, String title, String content, LocalDateTime createdAt, UserInquiryEntity inquiry) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.inquiry = inquiry;
+    }
+}
