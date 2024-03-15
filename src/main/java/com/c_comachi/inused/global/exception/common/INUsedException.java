d8414a5a03faae7c8425fa12bf69770d9e4a3e44
MoonJaeGyeong <bblbblan69@gmail.com>
2024-03-15 17:27:24 +0900

    :fire: delete : 에러 처리 코드 삭제

--- src/main/java/com/c_comachi/inused/global/exception/common/INUsedException.java
+++
@@ -1,9 +0,0 @@
-package com.c_comachi.inused.global.exception.common;
-
-public class INUsedException extends RuntimeException {
-
-    public INUsedException(String message) {
-        super(message);
-    }
-
-}
