c92ce679b3efd489db3e5cdb4dbcc4142623abf7
MoonJaeGyeong <bblbblan69@gmail.com>
2024-03-15 20:50:45 +0900

    :sparkles: feat : user 조회, 수정, 삭제 생성

--- src/main/java/com/c_comachi/inused/domain/users/controller/UserController.java
+++ src/main/java/com/c_comachi/inused/domain/users/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.c_comachi.inused.domain.users.controller;
 
-import com.c_comachi.inused.domain.users.dto.UserDto;
+import com.c_comachi.inused.domain.users.dto.request.UserEditRequestDto;
+import com.c_comachi.inused.domain.users.dto.response.GetLoginUserResponseDto;
+import com.c_comachi.inused.domain.users.dto.response.UserEditResponseDto;
 import com.c_comachi.inused.domain.users.service.UserService;
+import com.c_comachi.inused.global.dto.ResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +24,21 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/get/{email}")
-    @PreAuthorize("hasAnyRole('ROLE_USER')")
-    public ResponseEntity<UserDto> getLoginUser(@PathVariable(value = "email") String email) {
+    @GetMapping("/get")
+    public ResponseEntity<? super GetLoginUserResponseDto> getUser(Authentication authentication) {
+        ResponseEntity<? super GetLoginUserResponseDto> response = userService.getLoginUser(authentication.getName());
+        return response;
+    }
 
-        return ResponseEntity.ok(userService.getUserWithAuthorities(email));
+    @PatchMapping("/edit")
+    public ResponseEntity<ResponseDto> editUser(Authentication authentication, @RequestBody @Valid UserEditRequestDto requestDto) {
+        userService.editUser(authentication.getName(), requestDto.toUserEditInfo());
+        return ResponseDto.suc();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteUser(Authentication authentication) {
+        userService.deleteUser(authentication.getName());
+        return ResponseDto.suc();
     }
 }
