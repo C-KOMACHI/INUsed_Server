954ecbdfc0a108422db4335550aa4bcdb9c99841
MoonJaeGyeong <bblbblan69@gmail.com>
2024-03-15 16:35:46 +0900

    :sparkles: feat : User 조회 Dto 추가

---
+++ src/main/java/com/c_comachi/inused/domain/users/dto/response/GetLoginUserResponseDto.java
@@ -0,0 +1,36 @@
+package com.c_comachi.inused.domain.users.dto.response;
+
+import com.c_comachi.inused.domain.users.entity.UserEntity;
+import com.c_comachi.inused.global.common.ResponseCode;
+import com.c_comachi.inused.global.common.ResponseMessage;
+import com.c_comachi.inused.global.dto.ResponseDto;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class GetLoginUserResponseDto extends ResponseDto  {
+
+    private String email;
+    private String nickname;
+    private String profileImage;
+    private Double fireTemperature;
+
+    private GetLoginUserResponseDto(UserEntity userEntity){
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.email = userEntity.getEmail();
+        this.nickname = userEntity.getNickname();
+        this.profileImage = userEntity.getProfileImage();
+        this.fireTemperature = userEntity.getFireTemperature();
+    }
+
+    public static ResponseEntity<GetLoginUserResponseDto> success(UserEntity user) {
+        GetLoginUserResponseDto result = new GetLoginUserResponseDto(user);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+}
