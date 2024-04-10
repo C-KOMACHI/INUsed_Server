2b1ab7718fa2f0764e374429db5ad331c007e726
MoonJaeGyeong <bblbblan69@gmail.com>
2024-04-11 04:04:39 +0900

    :recycle: refactor : 날짜 순으로 최신 정렬

--- src/main/java/com/c_comachi/inused/domain/inquiry/repository/UserInquiryRepository.java
+++ src/main/java/com/c_comachi/inused/domain/inquiry/repository/UserInquiryRepository.java
@@ -15,7 +15,7 @@ public interface UserInquiryRepository extends JpaRepository<UserInquiryEntity, 
 
     Optional<UserInquiryEntity> findById(Long userInquiryId);
 
-    @Query("SELECT ui FROM UserInquiryEntity ui WHERE ui.user = :user")
+    @Query("SELECT ui FROM UserInquiryEntity ui WHERE ui.user = :user ORDER BY ui.createdAt DESC")
     List<UserInquiryEntity> findListByUser(@Param("user") UserEntity user);
 
 }
