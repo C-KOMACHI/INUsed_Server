aa917175847f17b1419fd25f31ad48125178fbce
yusica09 <choijiwoo0922@gmail.com>
2024-04-18 22:17:12 +0900

    :sparkles: feat : Post 전체 조회 Dto 생성

---
+++ src/main/java/com/c_comachi/inused/domain/post/dto/response/AllGetPostResponseDto.java
@@ -0,0 +1,31 @@
+package com.c_comachi.inused.domain.post.dto.response;
+
+import com.c_comachi.inused.domain.notice.dto.response.ViewNoticeResponseDto;
+import com.c_comachi.inused.domain.notice.entity.NoticeEntity;
+import com.c_comachi.inused.domain.post.entity.PostEntity;
+import com.c_comachi.inused.global.common.ResponseCode;
+import com.c_comachi.inused.global.common.ResponseMessage;
+import com.c_comachi.inused.global.dto.ResponseDto;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class AllGetPostResponseDto extends ResponseDto {
+    private List<PostEntity> posts;
+
+    private AllGetPostResponseDto(List<PostEntity> posts){
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.posts = posts;
+
+    }
+
+    public static ResponseEntity<? super AllGetPostResponseDto> success(List<PostEntity> posts){
+        AllGetPostResponseDto result = new AllGetPostResponseDto(posts);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
