d5e011d26ca9ecbfe1eac26182931e3088d1bd10
MoonJaeGyeong <bblbblan69@gmail.com>
2024-04-29 18:54:12 +0900

    :recycle: refactor : 코드 @Repository 어노테이션 추가

--- src/main/java/com/c_comachi/inused/domain/post/repository/CategoryRepository.java
+++ src/main/java/com/c_comachi/inused/domain/post/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.c_comachi.inused.domain.post.repository;
 
 import com.c_comachi.inused.domain.post.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
     Optional<CategoryEntity> findById(Long categoryId);
--- src/main/java/com/c_comachi/inused/domain/report/repository/UserReportRepository.java
+++ src/main/java/com/c_comachi/inused/domain/report/repository/UserReportRepository.java
@@ -2,7 +2,9 @@ package com.c_comachi.inused.domain.report.repository;
 
 import com.c_comachi.inused.domain.report.entity.UserReportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserReportRepository extends JpaRepository<UserReportEntity, Long> {
 
 }
