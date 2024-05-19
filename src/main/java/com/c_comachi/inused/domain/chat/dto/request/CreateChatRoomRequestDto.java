e2b3d90bb17cf52cded3264f04f1ac4694f8b670
MoonJaeGyeong <bblbblan69@gmail.com>
2024-05-19 17:21:25 +0900

    :recycle: refactor : 에러 Handler 코드 변경

--- src/main/java/com/c_comachi/inused/domain/chat/dto/request/CreateChatRoomRequestDto.java
+++ src/main/java/com/c_comachi/inused/domain/chat/dto/request/CreateChatRoomRequestDto.java
@@ -1,9 +1,12 @@
 package com.c_comachi.inused.domain.chat.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateChatRoomRequestDto {
+    @NotBlank
     private Long userId;
+    @NotBlank
     private Long postId;
 }
--- src/main/java/com/c_comachi/inused/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/c_comachi/inused/global/handler/GlobalExceptionHandler.java
@@ -7,6 +7,10 @@ import com.c_comachi.inused.global.common.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,19 +18,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE,
-//                e.getBindingResult());
-//        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-//    }
-//
-//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-//    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
-//            MethodArgumentTypeMismatchException e) {
-//        ErrorResponse response = ErrorResponse.of(e);
-//        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-//    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE,
+                e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
+        ErrorResponse response = ErrorResponse.of(e);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
